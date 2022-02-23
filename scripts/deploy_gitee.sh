e1363ad726f427bb93264aa1b0456956933bfcaf
heycn <x1chennan@qq.com>
2022-02-23 20:54:22 +0800

    init

---
+++ scripts/deploy_gitee.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "deploy" &&
+git remote add origin git@gitee.com:heycn/react-data-view.git &&
+git push -f -u origin "master" &&
+cd -
