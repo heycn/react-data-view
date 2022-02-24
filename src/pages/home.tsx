b0dfd93c7c8c595ad8edea275ef6507f0137f9d8
heycn <x1chennan@qq.com>
2022-02-24 13:48:02 +0800

    创建标题

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,10 @@ import './home.scss';
 export const Home = () => {
   return (
     <div>
-      <div className='x'></div>
+      <header>
+        <span className='title'>新型冠状病毒肺炎疫情分布</span>
+        <div className='tag'>免责声明：数据纯属虚构，真实数据请以官方为准！</div>
+      </header>
     </div>
   );
 };
