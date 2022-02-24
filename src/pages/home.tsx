21dc3f65b4554906ec168c004a114f854e9d926c
heycn <x1chennan@qq.com>
2022-02-24 15:11:21 +0800

    make grids

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,6 +8,13 @@ export const Home = () => {
         <span className='title'>新型冠状病毒肺炎疫情分布</span>
         <div className='tag'>免责声明：数据纯属虚构，真实数据请以官方为准！</div>
       </header>
+      <main>
+        <div className='section1'></div>
+        <div className='section2'></div>
+        <div className='section3'></div>
+        <div className='section4'></div>
+        <div className='section5'></div>
+      </main>
     </div>
   );
 };
