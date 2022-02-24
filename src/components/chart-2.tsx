8556fd3ac333e019e04496a78b03e733bc6da9e3
heycn <x1chennan@qq.com>
2022-02-24 23:59:38 +0800

    优化一下 css

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import * as echarts from 'echarts';
 import { px } from '../shared/px';
-import { baseEchartOptions } from '../shared/base-echart-options';
 import { createEchartsOptions } from '../shared/create-echarts-options';
 
 export const Chart2 = () => {
@@ -10,22 +9,16 @@ export const Chart2 = () => {
     var myChart = echarts.init(divRef.current);
     myChart.setOption(
       createEchartsOptions({
-        ...baseEchartOptions,
-        textStyle: {
-          fontSize: px(14),
-          color: '#fff'
-        },
-        title: { show: false },
-        legend: { show: false },
-        grid: {
-          x: px(80),
-          y: px(40),
-          x2: px(40),
-          y2: px(40)
-        },
         xAxis: {
           type: 'value',
-          boundaryGap: [0, 0.1]
+          boundaryGap: [0, 0.01],
+          splitLine: { show: false },
+          axisLabel: { show: false }
+        },
+        grid: {
+          x: px(60),
+          y: px(20),
+          y2: px(20)
         },
         yAxis: {
           type: 'category',
@@ -36,18 +29,13 @@ export const Chart2 = () => {
             name: '确诊确诊',
             type: 'bar',
             data: [15795570, 18734683, 22468239, 28485502, 42880507, 80366783]
-          },
-          {
-            name: '治愈',
-            type: 'bar',
-            data: [12836228, 17011869, 560486, 25772807, 42209519, 52453550]
           }
         ]
       })
     );
   }, []);
   return (
-    <div className='border 管辖统计'>
+    <div className='border 破获排名'>
       <h2>世界确诊病例排名</h2>
       <div ref={divRef} className='chart'></div>
     </div>
