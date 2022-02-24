80a87f6962b1c9ab9601a90bf3fe052149175b25
heycn <x1chennan@qq.com>
2022-02-24 14:18:23 +0800

    合并上次提交

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,7 @@ import './home.scss';
 
 export const Home = () => {
   return (
-    <div>
+    <div className='home'>
       <header>
         <span className='title'>新型冠状病毒肺炎疫情分布</span>
         <div className='tag'>免责声明：数据纯属虚构，真实数据请以官方为准！</div>
