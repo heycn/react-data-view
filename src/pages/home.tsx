c5d07ffb6efc3f8cea6fd84c00422b4526f002ba
heycn <x1chennan@qq.com>
2022-02-24 20:26:44 +0800

    重构chart-1

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,53 +1,8 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
+import { Chart1 } from '../components/chart-1';
 import './home.scss';
-import * as echarts from 'echarts';
-
-const px = n => (n / 2420) * (window as any).pageWidth;
 
 export const Home = () => {
-  const divRef = useRef(null);
-  useEffect(() => {
-    var myChart = echarts.init(divRef.current);
-    myChart.setOption({
-      textStyle: {
-        fontSize: px(14),
-        color: '#fff'
-      },
-      xAxis: {
-        data: ['待定', '待定', '待定', '待定', '待定'],
-        axisTick: { show: false },
-        axisLabel: {
-          fontSize: px(14),
-          formatter(val) {
-            if (val.length > 2) {
-              const array = val.split('');
-              array.splice(2, 0, '\n');
-              return array.join('');
-            } else {
-              return val;
-            }
-          }
-        }
-      },
-      grid: {
-        x: px(40),
-        y: px(40),
-        x2: px(40),
-        y2: px(40)
-      },
-      yAxis: {
-        axisLabel: {
-          fontSize: px(14)
-        }
-      },
-      series: [
-        {
-          type: 'bar',
-          data: [20, 36, 10, 10, 20]
-        }
-      ]
-    });
-  }, []);
   return (
     <div className='home'>
       <header>
@@ -56,10 +11,7 @@ export const Home = () => {
       </header>
       <main>
         <section className='section1'>
-          <div className='border 管辖统计'>
-            <h2>案发派出所管辖统计</h2>
-            <div ref={divRef} className='chart'></div>
-          </div>
+          <Chart1 />
         </section>
         <section className='border section2'></section>
         <section className='border section3'></section>
