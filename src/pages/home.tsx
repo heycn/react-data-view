1f0a4f8e90274295fa0c1b8d2a321335994d9f91
heycn <x1chennan@qq.com>
2022-02-25 15:21:35 +0800

    制作 Chart5

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,7 @@ import { Chart1 } from '../components/chart-1';
 import { Chart2 } from '../components/chart-2';
 import { Chart3 } from '../components/chart-3';
 import { Chart4 } from '../components/chart-4';
+import { Chart5 } from '../components/chart-5';
 import './home.scss';
 
 export const Home = () => {
@@ -20,7 +21,9 @@ export const Home = () => {
           <Chart3 />
           <Chart4 />
         </section>
-        <section className='border section3'></section>
+        <section className='border section3'>
+          <Chart5 />
+        </section>
         <section className='border section4'></section>
         <section className='border section5'></section>
       </main>
