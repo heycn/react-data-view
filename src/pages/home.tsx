29c6b928450adb2c688346b093dac7ccaa56dc79
heycn <x1chennan@qq.com>
2022-02-25 17:31:31 +0800

    完成chart-7

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,6 +6,7 @@ import { Chart4 } from '../components/chart-4';
 import { Chart5 } from '../components/chart-5';
 import { Chart6 } from '../components/chart-6';
 import './home.scss';
+import { Chart7 } from '../components/chart-7';
 
 export const Home = () => {
   return (
@@ -28,7 +29,21 @@ export const Home = () => {
         <section className='section4'>
           <Chart6 />
         </section>
-        <section className='border section5'></section>
+        <section className='section5'>
+          <div className='border row1 案发类型'>
+            <h2>案发类型统计</h2>
+            <div className='charts'>
+              <Chart7 />
+            </div>
+          </div>
+          <div className='border row2 案发街道'>
+            <h2>案发街道统计</h2>
+            <div className='charts'></div>
+          </div>
+          <div className='border row3 作案手段'>
+            <h2>作案手段分析</h2>
+          </div>
+        </section>
       </main>
       <footer>© heycn 2022 - 免责声明：本页数据仅供参考 请以官方数据为准</footer>
     </div>
