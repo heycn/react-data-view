d5fabbb43c3e78ca71b94d613684549bca4a2c74
heycn <x1chennan@qq.com>
2022-02-25 16:04:40 +0800

    优化

--- src/components/chart-6.tsx
+++ src/components/chart-6.tsx
@@ -6,7 +6,6 @@ import china from '../geo/china.json';
 
 export const Chart6 = () => {
   const divRef = useRef(null);
-  const colors = { 青海省: '#BB31F7', 甘肃省: '#15B8FD', 四川省: '#06E1EE' };
   useEffect(() => {
     var myChart = echarts.init(divRef.current);
     // @ts-ignore
@@ -19,39 +18,10 @@ export const Chart6 = () => {
           {
             type: 'map',
             mapType: 'CN', // 自定义扩展图表类型
-            data: [{ name: '甘肃省', value: 1 }],
-            label: { show: false, color: 'white' },
+            data: [{ name: '广东省', value: 100 }],
             itemStyle: {
               areaColor: '#010D3D',
-              color: colors['甘肃省'],
-              borderColor: '#01A7F7',
-              emphasis: {
-                label: { color: 'white' },
-                areaColor: '#5470C6'
-              }
-            }
-          },
-          {
-            type: 'map',
-            mapType: 'CN', // 自定义扩展图表类型
-            data: [{ name: '四川省', value: 100 }],
-            itemStyle: {
-              areaColor: '#010D3D',
-              color: colors['四川省'],
-              borderColor: 'yellow',
-              emphasis: {
-                label: { color: 'white' },
-                areaColor: '#5470C6'
-              }
-            }
-          },
-          {
-            type: 'map',
-            mapType: 'CN', // 自定义扩展图表类型
-            data: [{ name: '青海省', value: 100 }],
-            itemStyle: {
-              areaColor: '#010D3D',
-              color: colors['青海省'],
+              color: '#06E1EE',
               borderColor: '#01A7F7',
               emphasis: {
                 label: { color: 'white' },
@@ -69,14 +39,6 @@ export const Chart6 = () => {
       <h2>全市犯罪人员籍贯分布地</h2>
       <div className='wrapper'>
         <div ref={divRef} className='chart' />
-        <div className='legend bordered'>
-          <span className='icon' style={{ background: colors['甘肃省'] }} />
-          甘肃籍
-          <span className='icon' style={{ background: colors['四川省'] }} />
-          四川籍
-          <span className='icon' style={{ background: colors['青海省'] }} />
-          青海籍
-        </div>
         <div className='notes'>此地图仅显示了中国的部分区域</div>
       </div>
     </div>
